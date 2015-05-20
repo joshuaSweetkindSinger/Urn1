f888fd6f245567ac3f2e779b61e23f03bb87640b
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-05-20 08:57:05 -0700

    Alvin's first draft for solution with unknown composition

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-# This holds memoized state values.
+# States are represented as tuples of green/red marbles extracted respectively.
 values = {
-  (0, 0): 0
+  #(0, 0): 0
 }
 
-
-# Return the successor states to the specified state, along with their
-# associated transition probabilities.
-# The return value is an array of tuples of the form ((g', r'), p), where (g', r')
-# is a successor state of (g, r), and p is the probability of transitioning to it from (g, r).
-def successors(g, r):
-  states      = []
-  num_marbles = float(g + r)         # Need to convert to float for divisions below.
-
-  if g > 0:
-    p_green = g / num_marbles        # Probability of withdrawing a green marble from the urn.
-    states.append(((g-1, r), p_green))
-
-  if r > 0:
-    p_red = r / num_marbles          # Probability of withdrawing a red marble from the urn.
-    states.append(((g, r-1), p_red))
-
+# Returns not only successor states, but also probabilities (in 3ples).
+def successors(g, r, capacity):
+  states = []
+  if g + r != capacity:
+    num_extracted = g + r
+    num_compositions = capacity + 1 - g - r
+    num_remaining = capacity - num_extracted
+    p_green = float(sum(range(num_remaining))) / (num_compositions * num_remaining)
+    states.append((g+1, r, p_green))
+    p_red = 1 - p_green
+    states.append((g, r+1, p_red))
   return states
 
-
-# Calculate the value of the specified state, using memoization to retrieve previously calculated values.
-def value(g, r):
+def value(g, r, capacity):
+  stop_value = g - r
+  continuation_value = stop_value
+  if g + r != capacity:
+    successor_states = successors(g, r, capacity)
+    continuation_value = sum([value(s[0], s[1], capacity)*s[2] for s in successor_states])
   if (g, r) not in values:
-    continuation_value = sum([value(*s)*p for s, p in successors(g, r)])
-    stop_value         = g - r
-    values[(g, r)]     = max(stop_value, continuation_value)
-
+    values[(g, r)] = max(stop_value, continuation_value)
   return values[(g, r)]
 
-# Solve this particular board state.
-print value(2, 2)
+print value(0, 0, 20)
