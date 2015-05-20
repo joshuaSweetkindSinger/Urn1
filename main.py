04890bbf641e7b66c8b9533010a2d0782fae9a61
joshua sweetkind-singer <sweetkindsinger@gmail.com>
2015-05-20 08:30:19 -0700

    Reviewed, added documentation, and slightly refactored the code.
    In particular, note the new data representation for the return value
    of successors(), which returns an embedded tuple ((g, r), p).

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+# This holds memoized state values.
 values = {
   (0, 0): 0
 }
 
-# Successors returns not only successor states, but also probabilities (a 3ple).
+
+# Return the successor states to the specified state, along with their
+# associated transition probabilities.
+# The return value is an array of tuples of the form ((g', r'), p), where (g', r')
+# is a successor state of (g, r), and p is the probability of transitioning to it from (g, r).
 def successors(g, r):
-  states = []
-  num_marbles = float(g + r)
+  states      = []
+  num_marbles = float(g + r)         # Need to convert to float for divisions below.
+
   if g > 0:
-    p_green = g / num_marbles
-    states.append((g-1, r, p_green))
+    p_green = g / num_marbles        # Probability of withdrawing a green marble from the urn.
+    states.append(((g-1, r), p_green))
+
   if r > 0:
-    p_red = r / num_marbles
-    states.append((g, r-1, p_red))
+    p_red = r / num_marbles          # Probability of withdrawing a red marble from the urn.
+    states.append(((g, r-1), p_red))
+
   return states
 
+
+# Calculate the value of the specified state, using memoization to retrieve previously calculated values.
 def value(g, r):
   if (g, r) not in values:
-    continuation_value = sum([value(s[0], s[1])*s[2] for s in successors(g, r)])
-    stop_value = g - r
-    values[(g, r)] = max(stop_value, continuation_value)
+    continuation_value = sum([value(*s)*p for s, p in successors(g, r)])
+    stop_value         = g - r
+    values[(g, r)]     = max(stop_value, continuation_value)
+
   return values[(g, r)]
 
+# Solve this particular board state.
 print value(2, 2)
